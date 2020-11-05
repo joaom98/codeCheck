08fd21876ca2fe39b774392d0dadcaceddafe204
JoaoM <jm@pop-os.localdomain>
2020-11-05 14:19:04 -0300

    Modified to only run 1 case per container

--- task/python.sh
+++ task/python.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-number_of_inputs=$(ls input/ | wc -l)
-
-for test_case in $( seq 1 $number_of_inputs )
-do
-	python3 source_submitted/source.py < input/$test_case > output_generated/$test_case 2> output_generated/errors
-	echo "Test case output number ${test_case}: "
-	cat output_generated/$test_case	
-done
+python3 source_submitted/source.py < input/input > output_generated/output 2> output_generated/errors
+echo "Test case output: "
+cat output_generated/output	
 
 echo "Output files: "
 ls output_generated/
